b7b61fb7f0d7efe17f952fdab431b71a962a02b7
Bernardo-07 <bernardoduarte271@gmail.com>
2024-01-26 10:59:01 -0300

    pyaudio error solved

--- main.py
+++ main.py
@@ -5,5 +5,9 @@ r = sr.Recognizer()
 
 #abre e define o microfone como fonte de audio
 with sr.Microphone() as source:
+    #redução de ruido externo
+    r.adjust_for_ambient_noise(source)
+
+    print('Say something: ')
     audio = r.listen(source)
     print(r.recognize_google(audio))
